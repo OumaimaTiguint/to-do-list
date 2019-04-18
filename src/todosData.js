6d256e70a2dd3ccb51ee84d4cbaf8f4c1a411ef5
oumaima tiguint <oumaimatiguint@gmail.com>
2019-04-18 14:03:33 +0100

    first commit

---
+++ src/todosData.js
@@ -0,0 +1,24 @@
+const todosData = [
+    {
+        id:1,
+        text: "practice React",
+        completed: false
+    },
+    {
+        id:2,
+        text: "stretch",
+        completed: false
+    },
+    {
+        id:3,
+        text: "take notes",
+        completed: true
+    },
+    {
+        id:4,
+        text: "collect data",
+        completed: true
+    }
+]
+
+export default todosData
